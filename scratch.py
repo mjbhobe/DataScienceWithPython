395ab3bc0d38eec40ef05a25e5dc8035b7e9881a
Manish Bhobe <mjbhobe@gmail.com>
2023-11-18 10:34:25 +0530

    Code edits & bug fixes

--- scratch.py
+++ scratch.py
@@ -1,16 +1,25 @@
-from scipy.stats import norm
+# from scipy.stats import norm
 
-# find the area under std normal curve
-# to the **left** given a z score
+# # find the area under std normal curve
+# # to the **left** given a z score
 
-# with z = 1
-print(f"{norm.cdf(1):.3f}")
-# output: 0.841
+# # with z = 1
+# print(f"{norm.cdf(1):.3f}")
+# # output: 0.841
 
-# with z = 0.25
-print(f"{norm.cdf(0.25):.3f}")
-# 0.599
+# # with z = 0.25
+# print(f"{norm.cdf(0.25):.3f}")
+# # 0.599
 
-# with z = -0.45
-print(f"{norm.cdf(-0.45):.3f}")
-# 0.326
+# # with z = -0.45
+# print(f"{norm.cdf(-0.45):.3f}")
+# # 0.326
+
+
+from sklearn import datasets
+
+cancer_dataset = datasets.load_breast_cancer()
+print(
+    f"Data shape -> {cancer_dataset.data.shape} - "
+    f"target shape -> {cancer_dataset.target.shape}"
+)
